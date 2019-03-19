ed141a555bf4873eae7694d212a05c54cfcc911c
wxx9248 <wxx9248@qq.com>
2019-03-20 00:46:17 +0800

    2019/03/20 Uploaded

---
+++ clean-work-space.bat
@@ -0,0 +1,4 @@
+@del /F /Q /S *.aps *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res 
+@del /F /Q /S *.ncb *.opt *.suo *.manifest *.dep *.sdf *.tlog *.log *.ipch *.lastbuildstate
+
+@pause
