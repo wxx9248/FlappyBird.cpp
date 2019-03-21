edb55f7cd9745d2de43870823f9f2776287f6d32
wxx9248 <wxx9248@qq.com>
2019-03-21 21:58:06 +0800

    2019/03/21 Uploaded

--- clean-work-space.bat
+++ clean-work-space.bat
@@ -1,4 +1,4 @@
 @del /F /Q /S *.aps *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res 
 @del /F /Q /S *.ncb *.opt *.suo *.manifest *.dep *.sdf *.tlog *.log *.ipch *.lastbuildstate
-
+@del /F /Q "Flappy Bird\.vs\Flappy Bird\v15\Browse.VC.db"
 @pause
