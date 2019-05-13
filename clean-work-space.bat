39c6ac99d9e9325e42ca9acf299ab73f92b934b8
wxx9248 <wxx9248@qq.com>
2019-05-13 17:41:51 +0800

    2019/05/13 Uploaded

--- clean-work-space.bat
+++ clean-work-space.bat
@@ -1,9 +1,9 @@
-@echo off
+@ECHO OFF
 
-del /F /Q /S *.aps *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res 
-del /F /Q /S *.ncb *.opt *.suo *.manifest *.dep *.sdf *.tlog *.log *.ipch *.lastbuildstate
-del /F /Q "Flappy Bird\.vs\Flappy Bird\v15\Browse.VC.db"
-del /F /Q /S "x64"
-del /F /Q /S "Flappy Bird\x64"
+DEL /F /Q /S *.aps *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res 
+DEL /F /Q /S *.ncb *.opt *.suo *.manifest *.dep *.sdf *.tlog *.log *.ipch *.lastbuildstate
+DEL /F /Q "Flappy Bird\.vs\Flappy Bird\v15\Browse.VC.db"
+DEL /F /Q /S "x64"
+DEL /F /Q /S "Flappy Bird\x64"
 
-pause
+PAUSE
